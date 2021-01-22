ee93ceceb1c253f2d2b4381340f4827f23eb29b7
jordy muniz <jordy.muniz96@gmail.com>
2021-01-22 12:13:48 -0600

    exercise

---
+++ src/main/java/models/Quote.java
@@ -0,0 +1,34 @@
+package models;
+
+public class Quote {
+    private long id;
+    private Author author;
+    private String content;
+
+    public Quote() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
